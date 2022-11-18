760e67a118d35e8b733f5586c0d2b70a9f43cd2d
omg <oremgitonga22@gmail.com>
2022-11-19 02:17:21 +0300

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,58 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import NavBar from "../src/components/navbar/NavBar";
+import LeftBar from "../src/components/leftbar/LeftBar";
+import RightBar from "../src/components/rightbar/RightBar";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="login" />;
+    }
+    return children;
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
     {
       path: "/login",
---
+++ client/src/components/navbar/NavBar.jsx
@@ -0,0 +1,40 @@
+import "./NavBar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Let's Get Social</span>
+          <HomeOutlinedIcon />
+          <DarkModeOutlinedIcon />
+          <GridViewOutlinedIcon />
+          <div className="search">
+            <SearchOutlinedIcon />
+            <input type="text" placeholder="search" />
+          </div>
+        </Link>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="" alt="" />
+          <span>Orem G</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
